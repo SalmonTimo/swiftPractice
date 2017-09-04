5129dba8d43da77ef75a235f2ee4c5940a212585
Timothy Salmon <tsalmon2000@gmail.com>
2017-09-04 12:56:56 -0400

    Post assignent 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let twoThousand : UInt16 = 2000
+let one : UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
